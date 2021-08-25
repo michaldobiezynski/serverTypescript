95c9db7d2771520598c04303c283a969c9866ef4
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-08-25 09:01:37 +0100

    Wiring up sessions

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -24,10 +24,14 @@ router.get("/login", (req: Request, res: Response) => {
 
 router.post("/login", (req: RequestWithBody, res: Response) => {
   const { email, password } = req.body;
-  if (email) {
-    res.send(email.toUpperCase());
+  if (email && password && email === "hi@hi.com" && password === "password") {
+    // mark this person as logged in
+    req.session = { loggedIn: true };
+
+    // redirect them to the root route
+    res.redirect("/");
   } else {
-    res.send("You must provide an email.");
+    res.send("Invalid email or password");
   }
 });
 
