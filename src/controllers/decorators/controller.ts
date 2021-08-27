d6651e36c8fb19a0041ca2b79c076a1ef8b58587
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-08-27 22:39:52 +0100

    Automated Validation

--- src/controllers/decorators/controller.ts
+++ src/controllers/decorators/controller.ts
@@ -5,7 +5,20 @@ import "reflect-metadata";
 import { NextFunction, RequestHandler, Request, Response } from "express";
 
 function bodyValidators(keys: string): RequestHandler {
-  return function (req: Request, res: Response, next: NextFunction) {};
+  return function (req: Request, res: Response, next: NextFunction) {
+    if (!req.body) {
+      res.status(422).send("Invalid request");
+      return;
+    }
+
+    for (let key of keys) {
+      if (req.body[key]) {
+        return;
+      }
+    }
+
+    next();
+  };
 }
 
 export function controller(routePrefix: string) {
@@ -28,8 +41,19 @@ export function controller(routePrefix: string) {
         Reflect.getMetadata(MetadataKeys.middleware, target.prototype, key) ||
         [];
 
+      const requiredBodyProps =
+        Reflect.getMetadata(MetadataKeys.validator, target.prototype, key) ||
+        [];
+
+      const validator = bodyValidators(requiredBodyProps);
+
       if (path) {
-        router[method](`${routePrefix}${path}`, ...middlewares, routeHandler);
+        router[method](
+          `${routePrefix}${path}`,
+          ...middlewares,
+          validator,
+          routeHandler
+        );
       }
     }
   };
